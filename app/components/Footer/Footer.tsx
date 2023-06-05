6a9384511d7d8b675b4a17b8dd460af09dcffed0
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-06-05 15:42:42 +0530

    ;;;

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -54,7 +54,7 @@ const products: ProductType[] = [
 const footer = () => {
     return (
 
-        <div className="mx-auto max-w-2xl pt-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl sm:pt-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="my-12 grid grid-cols-1 gap-y-10 sm:grid-cols-6 lg:grid-cols-12">
 
                 {/* COLUMN-1 */}
