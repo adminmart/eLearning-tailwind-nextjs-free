1e6a73f810973baf05833ea75f5e22823cfd12cd
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-06-05 13:08:09 +0530

    ;;;

--- app/components/Navbar/Registerdialog.tsx
+++ app/components/Navbar/Registerdialog.tsx
@@ -56,7 +56,7 @@ const Register = () => {
                                             <div>
                                                 <img
                                                     className="mx-auto h-12 w-auto"
-                                                    src="/assets/logo/Logo.svg"
+                                                    src="/assets/logo/logo.svg"
                                                     alt="Your Company"
                                                 />
                                                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
